2c7497e8e38ed5a94c4509c6163052903d488ab9
bensonirah <bensonirah@gmail.com>
2022-03-27 21:21:47 +0300

    finish useCallBack hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,27 @@
-import { useReducer } from "react";
+import { useCallback, useReducer, useState } from "react";
 import "./App.css";
-
-const initialTodos = [
-  {
-    id: 1,
-    title: "Todo 1",
-    complete: false,
-  },
-  {
-    id: 2,
-    title: "Todo 2",
-    complete: false,
-  },
-];
-
-// Should put inside service
-const completeTask = (taskId, todos = []) => {
-  return todos.map((todo) => {
-    return todo.id === taskId ? { ...todo, complete: !todo.complete } : todo;
-  });
-};
-
-// Should have reducer per domain e.g: User CRUD reducer
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "COMPLETE":
-      return completeTask(action.id, state);
-    default:
-      return state;
-  }
-};
+import Todos from "./Todos";
 
 function App() {
-  const [todos, dispatch] = useReducer(reducer, initialTodos);
-  const handleComplete = (todo) => {
-    dispatch({ type: "COMPLETE", id: todo.id });
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+  
+  const increment = () => {
+    setCount((c) => c + 1);
   };
+
+  const addTodo = useCallback(() => {
+    setTodos((todos) => [...todos, "New todo"]);
+  }, [Todos]);
 
   return (
     <>
-      {todos.map((todo) => (
-        <div key={todo.id}>
-          <label>
-            <input
-              type="checkbox"
-              checked={todo.complete}
-              onChange={() => handleComplete(todo)}
-            />
-            {todo.title}
-          </label>
-        </div>
-      ))}
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>Increment</button>
+      </div>
     </>
   );
 }
